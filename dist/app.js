04ae79c42bddfa647943af4f01dff18abc2a147e
Rigoberto Carmenate <rigo811218@gmail.com>
2020-12-01 11:58:20 -0500

    Proyecto terminado

--- dist/app.js
+++ dist/app.js
@@ -29,9 +29,26 @@ const PintarDB = () => {
         arrayActividades = []
     }else{
         arrayActividades.forEach(element => {
-            listaActividadesUI.innerHTML += `<div class="alert alert-danger" role="alert"><span class="material-icons float-left mr-2">accessibility_new</span><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><span class="material-icons">done</span><span class="material-icons">delete</span></span></div>`
+            listaActividadesUI.innerHTML += `<div class="alert ${element.estado===true ? 'alert-success' : 'alert-danger'}" role="alert"><span class="material-icons float-left mr-2">accessibility_new</span><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><span class="material-icons">done</span><span class="material-icons">delete</span></span></div>`
         });
     }
+}
+
+const EliminarDB = (actividad) => {
+    let indexArray
+    arrayActividades.forEach((elemento, index) => {
+        if(elemento.actividad === actividad){
+            indexArray = index
+        }
+    })
+    arrayActividades.splice(indexArray, 1)
+    GuardarDB()
+}
+
+const EditarDB = (actividad) => {
+    let indexArray = arrayActividades.findIndex( (elemento) => elemento.actividad===actividad )
+    arrayActividades[indexArray].estado = true
+    GuardarDB()
 }
 
 //EventListener
@@ -51,6 +68,13 @@ listaActividadesUI.addEventListener('click', (e) => {
     e.preventDefault()
 
     if(e.target.innerHTML === "done" || e.target.innerHTML === "delete"){
-        console.log(e.target.offsetParent.childNodes[1].innerHTML)
+        let texto = e.target.offsetParent.childNodes[1].innerHTML
+        if(e.target.innerHTML === "done"){
+            EditarDB(texto)
+        }
+
+        if(e.target.innerHTML === "delete"){
+            EliminarDB(texto)
+        }
     }
 })
